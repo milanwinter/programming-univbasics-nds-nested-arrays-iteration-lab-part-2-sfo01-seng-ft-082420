43859fff6070b03cced9b550838793253e00fe6a
Milan Winter <milanwinter8@gmail.com>
2020-07-31 20:34:26 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,9 +3,9 @@ def find_min_in_nested_arrays(src)
   new_array = []
   while count < src.length do
     inner = 0
-    minimum = 0
+    minimum = 1000
     while inner < src[count].length do
-      if minimum < src[count][inner]
+      if minimum > src[count][inner]
         minimum = src[count][inner]
         end
       inner += 1
